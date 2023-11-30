bf436df183dc9bdb4c41bb05dfc3150a5ce91714
Jake Newton <jake.newt@0dark30.com>
2023-11-29 22:08:44 -0700

    Added Meta Tags

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 window.addEventListener("DOMContentLoaded", function () {
-   /*
-   document.querySelector("#fetchQuotesBtn").addEventListener("click", function () {
-
-      // Get values from drop-downs
-      const topicDropdown = document.querySelector("#topicSelection");
-      const selectedTopic = topicDropdown.options[topicDropdown.selectedIndex].value;
-      const countDropdown = document.querySelector("#countSelection");
-      const selectedCount = countDropdown.options[countDropdown.selectedIndex].value;
    
-      // Get and display quotes
-      fetchQuotes(selectedTopic, selectedCount);
-   });
-   */
 });
 
 let timerId = setTimeout(showSale, 3000);
